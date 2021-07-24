2dbd20ce651c2446cc175673b13548ad64a8d9e2
joy <56821389+joyjoyful92@users.noreply.github.com>
2021-07-24 16:33:33 +0900

    Add files via upload

---
+++ src/main/java/joy/studying/toy01/Toy01Application.java
@@ -0,0 +1,13 @@
+package joy.studying.toy01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toy01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toy01Application.class, args);
+	}
+
+}
